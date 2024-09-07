c98370b2cf94a9fcade600fb9231c4f9b476fb4e
Alexander <alexander.ochmanowicz@temple.edu>
2024-09-07 02:45:31 -0400

    fixed log

--- src/log.h
+++ src/log.h
@@ -1,9 +1,9 @@
 #ifndef LOGGER
 #define LOGGER
 
-#define INFO(message, ...) output(INFO, message, __VA_ARGS__);
-#define WARNING(message, ...) output(WARNING, message, __VA_ARGS__);
-#define ERROR(message, ...) output(ERROR, message, __VA_ARGS__);
+#define INFO(message, ...) output(INFO, message, ##__VA_ARGS__);
+#define WARNING(message, ...) output(WARNING, message, ##__VA_ARGS__);
+#define ERROR(message, ...) output(ERROR, message, ##__VA_ARGS__);
 
 enum log_level {
   INFO,
